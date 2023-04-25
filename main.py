538a04148910ad3e7ca596d8e2ea4f7fd5e0013a
Jie Lyu <jielyu@Jies-MacBook-Pro.local>
2023-04-25 09:11:31 -0600

    change back dvc

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ from starter.ml.model import compute_model_metrics
 
 if "DYNO" in os.environ and os.path.isdir(".dvc"):
     os.system("dvc config core.no_scm true")
-    # if os.system("dvc pull") != 0:
-    #     exit("dvc pull failed")
-    # os.system("rm -r .dvc .apt/usr/lib/dvc")
+    if os.system("dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
 
 app = FastAPI()
 
